12a0efaec121cff70b7593e4c0505ac73eb4e23c
flowersun99 <102143406+flowersun99@users.noreply.github.com>
2022-07-31 19:56:30 +0900

    Update realfinish.py

--- realfinish.py
+++ realfinish.py
@@ -11,21 +11,21 @@ import pandas as pd
 import numpy as np
 
 
-# 사전 정보 정의
+# pre information 
 username = #write your instagram id
 userpw = #write your instagram password
 time.sleep(3)
 
 
-# 해시태그 url 값
+# your goal url 값
 url = 'https://www.instagram.com/kittybunnypony/?igshid=YmMyMTA2M2Y%3D' #this is my friend's page so, click like and write command!
 
 
-# 인스타 로그인 URL
+# login URL
 loginUrl = 'https://www.instagram.com/accounts/login/'
 
 # Chrome drvier 실행
-driver = wd.Edge("C:/Users/jean8/OneDrive/바탕 화면/python workspace/msedgedriver")
+driver = wd.Edge("C:/Users/jean8/OneDrive/바탕 화면/python workspace/msedgedriver") # write your msedgedriver's path
 driver.maximize_window()
 driver.get(loginUrl)
 time.sleep(3)
@@ -42,11 +42,11 @@ time.sleep(3)
 
 driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.PAGE_DOWN)
 time.sleep(2)
-# 맨 왼쪽 상단 첫 게시물 클릭
+# click first posting
 driver.find_element(By.CSS_SELECTOR, 'div._aabd._aa8k._aanf').click()
 time.sleep(3)
 
 for i in range(200):     
-    # 최대 5초까지 기다렸다가, > 모양 클릭하여 다음 게시물로 넘어가기
+    # to click next posting
     driver.find_element(By.CSS_SELECTOR, 'div._aaqg._aaqh').click()
     time.sleep(3)
